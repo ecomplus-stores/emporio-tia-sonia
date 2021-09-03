1c73582ed64960f5fd3b17f108233acabc74702e
Leonardo Matos <leomp120894@gmail.com>
2021-09-03 10:43:26 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -2,7 +2,7 @@
 !function(a){a.fn.equalHeights=function(){var b=0,c=a(this);return c.each(function(){var c=a(this).innerHeight();c>b&&(b=c)}),c.css("height",b)},a("[data-equal]").each(function(){var b=a(this),c=b.data("equal");b.find(c).equalHeights()})}(jQuery);
 
 $('.header__search-input').keyup(function(){
-    $('body .search__input').val($(this).val()).[0].dispatchEvent(new Event('input'));
+    $('body .search__input').val($(this).val())[0].dispatchEvent(new Event('input'));
 });
 $(document).ready(function(){
     $('.product-card__name').equalHeights();
