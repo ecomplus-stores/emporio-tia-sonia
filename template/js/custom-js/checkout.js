680259915d093c45fef3357b0ca055df9c646576
lojatiasonia <54675200+lojatiasonia@users.noreply.github.com>
2021-09-15 19:53:06 -0300

    Update checkout.js

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -8,10 +8,10 @@ routerToLink.afterEach(({ name }) => {
       if (pedido && statusOrderPaid) {
         const timeShipping = document.querySelector('.shipping-line')
         const shippingtTracking = document.querySelector('.order-info__shipping-tracking')
-        const htmlLink = '<br><a href="/acompanhar-pedido" target="blank">Acompanhar Pedido</a>'
-        
+               
         timeShipping.insertAdjacentHTML('afterend', htmlLink)
         clearInterval(tryAppendInterval)
+        const htmlLink = '<br><a href="/acompanhar-pedido" target="blank">Acompanhar Pedido</a>'
       }
     }
     const tryAppendInterval = setInterval(appendLink, 200)
