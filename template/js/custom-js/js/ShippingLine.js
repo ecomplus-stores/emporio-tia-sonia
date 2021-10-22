ef420cef355a70330ba14af99f652e1401de6850
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-10-22 12:04:40 -0300

    feat(shippingLine): change days to date limit

--- template/js/custom-js/js/ShippingLine.js
+++ template/js/custom-js/js/ShippingLine.js
@@ -1,25 +1,35 @@
 import {
   i19days,
+  i19free,
   i19freeShipping,
+  // i19pickUpToday,
+  i19receiveToday,
+  i19untilTomorrow,
   i19upTo,
   i19workingDays
 } from '@ecomplus/i18n'
-
+  
 import {
   i18n,
   formatMoney
 } from '@ecomplus/utils'
-
+  
+const i19pickUpToday = 'Retire hoje'
+  
 export default {
   name: 'ShippingLine',
-
+  
   props: {
     shippingLine: {
       type: Object,
       required: true
+    },
+    productionDeadline: {
+      type: Number,
+      default: 0
     }
   },
-
+  
   computed: {
     deadlineStr () {
       const shipping = this.shippingLine
@@ -29,17 +39,23 @@ export default {
       if (shipping.delivery_time) {
         days += shipping.delivery_time.days
       }
-      return `${i18n(i19upTo)} ${days} ${i18n(isWorkingDays ? 'Dia(s) úteis' : i19days)}`
+      days += this.productionDeadline
+      console.log(days)
+      let date = new Date()
+      let today = date.getDay()
+      date.setDate(date.getDate() + days + (today === 6 ? 2 : +!today) + (Math.floor((days - 1 + (today % 6 || 1)) / 5) * 2));
+      return 'Até ' + date.toLocaleDateString();
     },
-
+  
     freightValueStr () {
-      const freight = typeof this.shippingLine.total_price === 'number'
-        ? this.shippingLine.total_price
-        : this.shippingLine.price
+      const { shippingLine } = this
+      const freight = typeof shippingLine.total_price === 'number'
+        ? shippingLine.total_price
+        : shippingLine.price
       if (freight) {
         return formatMoney(freight)
       } else {
-        return i18n(i19freeShipping)
+        return i18n(shippingLine.pick_up ? i19free : i19freeShipping)
       }
     }
   }
