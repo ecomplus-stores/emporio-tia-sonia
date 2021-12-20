128fca782cf3d8aec9a1caafbda1631e299393f9
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-12-20 14:02:26 -0300

    chore: update container calc

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -1,2 +1,33 @@
 import '#template/js/checkout'
 import './custom-js/checkout'
+import ecomCart from '@ecomplus/shopping-cart'
+const lessUnit = document.getElementById('lessUnit')
+const firstphrase = document.getElementById('lessSome')
+const lastphrase = document.getElementById('noMore')
+const lessQuantity = 199
+lessUnit.innerHTML = window.ecomUtils.formatMoney(lessQuantity, 'BRL', 'pt_br')
+ecomCart.on('change', ({ data }) => {
+  const cartCalc = document.querySelectorAll('#cart')
+  if (cartCalc.length) {
+    document.getElementById('containerCalc').style.display = 'none'
+    let percentBar
+    const countQuantity = data.subtotal
+    const evalQuantity = lessQuantity - countQuantity
+    if (evalQuantity > 0) {
+      lessUnit.innerHTML = window.ecomUtils.formatMoney(evalQuantity, 'BRL', 'pt_br')
+      percentBar = Math.round(countQuantity / lessQuantity * 100) + '%'
+      document.getElementById('lastUnitsBar').style.width = percentBar
+      document.getElementById('percentBar').innerHTML = percentBar
+      firstphrase.style.display = 'block'
+      lastphrase.style.display = 'none'
+    } else {
+      percentBar = '100%'
+      firstphrase.style.display = 'none'
+      lastphrase.style.display = 'block'
+      document.getElementById('lastUnitsBar').style.width = percentBar
+      document.getElementById('percentBar').innerHTML = percentBar
+    }
+  } else {
+    document.getElementById('containerCalc').style.display = 'none'
+  }
+})
