578b678d398df88d5a85e67c25fe807ff55aa7a8
eduvlemes <edu.vlemes@gmail.com>
2022-01-27 16:36:44 -0300

    seletor quantidade pagina de produto

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -5,6 +5,7 @@ module.exports = () => ({
     alias: {
       './js/ShippingLine.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingLine.js'),
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
+      './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.js'),
       './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/html/APrices.html')
     }
   }
