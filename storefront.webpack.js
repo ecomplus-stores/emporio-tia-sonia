77140e030fb687f82cabb3ea943be6ac3aa7fe5d
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2022-02-21 21:52:54 -0300

    Pontos teste

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -6,7 +6,8 @@ module.exports = () => ({
       './js/ShippingLine.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingLine.js'),
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
       './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.js'),
-      './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/html/APrices.html')
+      './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/html/APrices.html'),
+      './html/PointsApplier.js': path.resolve(__dirname, 'template/js/custom-js/html/PointsApplier.js')
     }
   }
 })
