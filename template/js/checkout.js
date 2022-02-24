ea2a9f03a1660f522624e4171ed7b4b3597dd39d
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2022-02-24 19:08:46 -0300

    chore: update cashback box

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -5,7 +5,7 @@ const lessUnit = document.getElementById('lessUnit')
 const firstphrase = document.getElementById('lessSome')
 const lastphrase = document.getElementById('noMore')
 const lessQuantity = 150
-if(lessUnit.length) {
+if(lessUnit) {
   lessUnit.innerHTML = window.ecomUtils.formatMoney(lessQuantity, 'BRL', 'pt_br')
   ecomCart.on('change', ({ data }) => {
     const cartCalc = document.querySelectorAll('#cart')
@@ -33,3 +33,28 @@ if(lessUnit.length) {
     }
   })
 }
+
+storefront.on('widget:@ecomplus/widget-tag-manager', () => {
+  setTimeout(() => {
+      if((window.innerWidth < 767) && $('#cart').length) {
+          $('.cart__discount').after($('.recommended-items'))
+      }
+      const $points = document.querySelector('.prices__points')
+      let points = document.querySelector('.prices__points span').innerText.replace('+', '').trim()
+      let transformToMoney = (Number(points) * 0.05).toFixed(2)
+      const $money =  `<div id="dinheiro-volta"><span><strong>Ganhe</strong> <span class="dinheiro">R$ <strong id="changeMoney">${transformToMoney}</strong></span> <br> em cashback na sua próxima compra</span></div>`
+      const $div = document.createElement('div')
+      $div.id = 'cashback'
+      $div.insertAdjacentElement('afterbegin', $points)
+      $div.insertAdjacentHTML('afterbegin', $money)
+      $('.cart__list').after($div)
+      ecomCart.on('change', ({ data }) => {
+        setTimeout(() => {
+          points = document.querySelector('.prices__points span').innerText.replace('+', '').trim()
+          transformToMoney = (Number(points) * 0.05).toFixed(2)
+          document.getElementById('changeMoney').innerText = transformToMoney
+        }, 800)
+      })
+      
+  }, 800);
+});
--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -22,20 +22,3 @@ routerToLink.afterEach(({ name }) => {
   }
 })
 
-storefront.on('widget:@ecomplus/widget-tag-manager', () => {
-    setTimeout(() => {
-        if((window.innerWidth < 767) && $('#cart').length) {
-            $('.cart__discount').after($('.recommended-items'))
-        }
-        const $points = document.querySelector('.prices__points')
-        const points = document.querySelector('.prices__points span').innerText.replace('+', '').trim()
-        const transformToMoney = (Number(points) * 0.05).toFixed(2)
-        const $money =  `<div id="dinheiro-volta"><span><strong>Ganhe</strong> <span class="dinheiro">R$ ${transformToMoney}</span> <br> em cashback na sua próxima compra</span></div>`
-        const $div = document.createElement('div')
-        $div.id = 'cashback'
-        $div.insertAdjacentElement('afterbegin', $points)
-        $div.insertAdjacentHTML('afterbegin', $money)
-        $('.cart__list').after($div)
-        
-    }, 800);
-  });
