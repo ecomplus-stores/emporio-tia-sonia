9a6599ec38dad81a4918b93d4179e333fde2fe0f
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2022-07-14 12:39:37 -0300

    chore: change form of points view

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -120,16 +120,11 @@ else {
 if (window.storefront && window.storefront.context && window.storefront.context.resource === 'products') {
   storefront.on('widget:@ecomplus/widget-tag-manager', function () {
     setTimeout(function () {
-      const $points = document.querySelector('.prices__points')
-      let points = document.querySelector('.prices__points span').innerText.replace('+', '').trim()
+      const $points = document.querySelector('.product__prices .prices__points')
+      let points = document.querySelector('.product__prices .prices__points span').innerText.replace('+', '').trim()
       let transformToMoney = (Number(points) * 0.05).toFixed(2)
-      console.log(transformToMoney)
-      const $money =  `<div id="dinheiro-volta"><span>Ganhe <span class="dinheiro">R$ <strong id="changeMoney">${new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(transformToMoney)}</strong></span> na próxima compra</span></div>`
-      const $div = document.createElement('div')
-      $div.id = 'cashback'
-      $div.insertAdjacentHTML('afterbegin', $money)
-      console.log($div)
-      $('.prices__points').after(`<div id="dinheiro-volta"><span>Ganhe <span class="dinheiro"><strong id="changeMoney">${new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(100.80)}</strong></span> na próxima compra</span></div>`)
+      const $money =  `<div id="dinheiro-volta"><span>Ganhe <span class="dinheiro"><strong nid="changeMoney">${new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(transformToMoney)}</strong></span> na próxima compra</span></div>`
+      $('.product__sale-timer').before($money)
     }, 800);
   });
 }
