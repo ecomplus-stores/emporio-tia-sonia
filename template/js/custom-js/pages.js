1bf092bddd748b6b6125d44b0598ce199449e4b1
Matheus Reis <matheusgnreis@gmail.com>
2022-08-05 10:04:44 -0300

    fix: buy on product

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -15,7 +15,7 @@ if (window.storefront && window.storefront.context && window.storefront.context.
 }
 
 !function(a){a.fn.equalHeights=function(){var b=0,c=a(this);return c.each(function(){var c=a(this).innerHeight();c>b&&(b=c)}),c.css("height",b)},a("[data-equal]").each(function(){var b=a(this),c=b.data("equal");b.find(c).equalHeights()})}(jQuery);
-
+//jquery
 $('.header__search-input').keyup(function(){
     $('body .search__input').val($(this).val())[0].dispatchEvent(new Event('input'));
 });
