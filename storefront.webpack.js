18b8723df815374e87c2269a3d68d875beee39ac
Leonardo Matos <leomp120894@gmail.com>
2022-11-18 22:50:49 -0300

    Moving custom `APrices.html` file

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -4,7 +4,7 @@ module.exports = () => ({
   resolve: {
     alias: {
       './js/ShippingLine.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingLine.js'),
-      './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/html/APrices.html'),
+      './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/components/APrices.html'),
       './html/PointsApplier.html': path.resolve(__dirname, 'template/js/custom-js/html/PointsApplier.html'),
       './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductCard.html'),
       './js/ProductCard.js': path.resolve(__dirname, 'template/js/custom-js/html/ProductCard.js'),
