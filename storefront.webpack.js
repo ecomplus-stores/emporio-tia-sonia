4fbef2517cf7da2633d897141b04de00460f8a5d
Leonardo Matos <leomp120894@gmail.com>
2022-11-21 09:54:32 -0300

    Update storefront.webpack.js

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -4,12 +4,12 @@ module.exports = () => ({
   resolve: {
     alias: {
       './js/ShippingLine.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingLine.js'),
-      './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/components/APrices.html'),
+      // './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/components/APrices.html'),
       './html/PointsApplier.html': path.resolve(__dirname, 'template/js/custom-js/html/PointsApplier.html'),
       './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductCard.html'),
       './js/ProductCard.js': path.resolve(__dirname, 'template/js/custom-js/html/ProductCard.js'),
-      './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.html'),
-      './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.js'),
+      // './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.html'),
+      // './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.js'),
       './html/EcOrderInfo.html': path.resolve(__dirname, 'template/js/custom-js/html/EcOrderInfo.html')
       //'./js/LoginBlock.js': path.resolve(__dirname, 'template/js/custom-js/js/LoginBlock.js')
     }
