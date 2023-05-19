6108d4429d7532801a7895f6a30e889c4fadea67
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-05-19 20:51:09 -0300

    chore: remove discount applier

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -6,7 +6,6 @@ module.exports = () => ({
       './js/ShippingLine.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingLine.js'),
       './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/components/APrices.html'),
       './html/DiscountApplier.html': path.resolve(__dirname, 'template/js/custom-js/components/DiscountApplier.html'),
-      './js/DiscountApplier.js': path.resolve(__dirname, 'template/js/custom-js/components/DiscountApplier.js'),
       './html/PointsApplier.html': path.resolve(__dirname, 'template/js/custom-js/html/PointsApplier.html'),
       './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductCard.html'),
       './js/ProductCard.js': path.resolve(__dirname, 'template/js/custom-js/html/ProductCard.js'),
