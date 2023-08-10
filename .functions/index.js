e88e134f5a38353199add0dce74c07a510689545
Leonardo Matos <leomp120894@gmail.com>
2023-08-10 18:48:59 -0300

    Functions v2

--- .functions/index.js
+++ .functions/index.js
@@ -1,7 +1,27 @@
-const functions = require('firebase-functions')
+const { initializeApp } = require('firebase-admin/app')
+const { onRequest } = require('firebase-functions/v2/https')
 
 const { ssr } = require('@ecomplus/storefront-renderer/functions/')
 
 process.env.STOREFRONT_LONG_CACHE = 'true'
 
-exports.ssr = functions.https.onRequest((req, res) => ssr(req, res))
+initializeApp()
+
+exports.ssr2 = onRequest({
+  concurrency: 80,
+  minInstances: 0,
+  memory: '1GiB',
+}, async (req, res) => {
+  if (req.path.startsWith('/~partytown')) {
+    res.sendStatus(404)
+    return null
+  }
+  res.set('x-load-took', '1')
+  return ssr(req, res)
+})
+
+exports.reverseproxy = onRequest({
+  concurrency: 80,
+  minInstances: 0,
+  memory: '1GiB',
+}, ssr)
