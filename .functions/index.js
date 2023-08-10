34879f31615bbea18f2720c357605da30bfa9289
Leonardo Matos <leomp120894@gmail.com>
2023-08-10 18:56:11 -0300

    Functions v2 and Hosting for SWR

--- .functions/index.js
+++ .functions/index.js
@@ -25,7 +25,3 @@ exports.reverseproxy = onRequest({
   minInstances: 0,
   memory: '1GiB',
 }, ssr)
-
-const { ssr } = require('@ecomplus/storefront-renderer/functions/')
-
-exports.ssr = functions.https.onRequest((req, res) => ssr(req, res))
