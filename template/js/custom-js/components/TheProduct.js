3c7e6f98e0cd7ff3e8b82a712d77f5d990abab6c
matheusgnreis <matheus@e-com.club>
2023-11-20 16:29:08 -0300

    chore: update sale timer

--- template/js/custom-js/components/TheProduct.js
+++ template/js/custom-js/components/TheProduct.js
@@ -240,13 +240,24 @@ export default {
         : 0
     },
 
-    isOnSale () {
+    mockNewPromoDate () {
       const { body } = this
+      const newPromoDate = { ...body }
+      const tomorrow = new Date(new Date().getTime() + 86400000).setHours(0, 0, 0, 0)
+      newPromoDate.price_effective_date = {}
+      newPromoDate.price_effective_date.end = new Date(tomorrow).toISOString()
+      console.log(newPromoDate)
+      console.log(checkOnPromotion(newPromoDate))
+      return newPromoDate
+    },
+
+    isOnSale () {
+      const { mockNewPromoDate } = this
       return this.hasPromotionTimer &&
-        checkOnPromotion(body) &&
-        body.price_effective_date &&
-        body.price_effective_date.end &&
-        Date.now() < new Date(body.price_effective_date.end).getTime()
+        (checkOnPromotion(mockNewPromoDate) || this.body.price > this.fixedPrice) &&
+        mockNewPromoDate.price_effective_date &&
+        mockNewPromoDate.price_effective_date.end &&
+        Date.now() < new Date(mockNewPromoDate.price_effective_date.end).getTime()
     },
 
     ghostProductForPrices () {
@@ -598,7 +609,7 @@ export default {
       setStickyBuyObserver()
     }
     if (this.isOnSale) {
-      const promotionDate = new Date(this.body.price_effective_date.end)
+      const promotionDate = new Date(this.mockNewPromoDate.price_effective_date.end)
       const now = Date.now()
       if (promotionDate.getTime() > now) {
         let targetDate
