e393f888e536c3ab3c0cbc54d6cc24887d120fbe
matheusgnreis <matheus@e-com.club>
2024-01-19 12:07:55 -0300

    chore: replace price by kit term in cart item

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -10,6 +10,8 @@ module.exports = () => ({
       './js/DiscountApplier.js': path.resolve(__dirname, 'template/js/custom-js/components/DiscountApplier.js'),
       './html/PointsApplier.html': path.resolve(__dirname, 'template/js/custom-js/html/PointsApplier.html'),
       './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductCard.html'),
+      './html/CartItem.html': path.resolve(__dirname, 'template/js/custom-js/html/CartItem.html'),
+      './html/EcSummary.html': path.resolve(__dirname, 'template/js/custom-js/html/EcSummary.html'),
       './js/ProductCard.js': path.resolve(__dirname, 'template/js/custom-js/html/ProductCard.js'),
       './html/ProductGallery.html': path.resolve(__dirname, 'template/js/custom-js/components/ProductGallery.html'),
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.html'),
