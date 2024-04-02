1aa5e2603c2e55da3d58d14916719b5c99a9c958
matheusgnreis <matheus@e-com.club>
2024-04-02 02:06:18 -0300

    chore: update store

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -242,3 +242,32 @@ else {
   //info.textContent = "Your Browser does not support Speech Recognition";
 }
 
+if (window.ecomPassport.checkLogin()) {
+  const customer = ecomPassport.getCustomer()
+  const customerPurchaseData = {}
+  let shippingAddr
+  if (customer) {
+    customerPurchaseData.customerDisplayName = window.ecomUtils.nickname(customer)
+    if (customer.name) {
+      customerPurchaseData.customerGivenName = customer.name.given_name
+      customerPurchaseData.customerFamilyName = customer.name.family_name
+    }
+    customerPurchaseData.customerEmail = customer.main_email
+    customerPurchaseData.customerPhone = window.ecomUtils.phone(customer)
+    shippingAddr = customer.addresses && customer.addresses[0]
+    if (shippingAddr && shippingAddr.zip) {
+      customerPurchaseData.shippingAddrZip = shippingAddr.zip
+      customerPurchaseData.shippingAddrStreet = shippingAddr.street
+      customerPurchaseData.shippingAddrNumber = shippingAddr.number
+      if (shippingAddr.street && shippingAddr.number) {
+        customerPurchaseData.shippingAddrStreet += `, ${shippingAddr.number}`
+      }
+      customerPurchaseData.shippingAddrCity = shippingAddr.city
+      customerPurchaseData.shippingAddrProvinceCode = shippingAddr.province_code
+    }
+    window.dataLayer.push({
+      event: 'customerExtraData',
+      ...customerPurchaseData
+    })
+  }
+}
